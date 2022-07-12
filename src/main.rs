c4061b5a16c9a3bd091c5b0cf9230264c8b28c7d
heidtmare <heidtmare@gmail.com>
2022-07-12 15:56:56 -0400

    playing with config options

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{
+    dev::{HttpServiceFactory, ServiceFactory},
+    get, web, App, HttpServer, Responder,
+};
+use actix_web_lab::web::{self as web_lab, Redirect};
+
+#[get("/health")]
+async fn health() -> impl Responder {
+    format!("ok")
+}
+
+#[actix_web::main] // or #[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new()
+    .service(health)
+    .service(load_services()))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
+
+fn load_services() -> Vec<Redirect> {
+    vec![
+        web_lab::Redirect::new("/cats", "http://cats.com"),
+        web_lab::Redirect::new("/dogs", "http://dogs.com"),
+    ]
 }
